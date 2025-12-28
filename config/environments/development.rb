0d857dbfa99339a4d8536b3d76be3229c6f35af8
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-28 21:02:40 +0500

    Added smtp settings for email authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,11 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
+  
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -59,7 +55,22 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'], # your-email@gmail.com
+    password: ENV['GMAIL_APP_PASSWORD'], # App password (not regular password)
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
 
   # Raises error for missing translations.
