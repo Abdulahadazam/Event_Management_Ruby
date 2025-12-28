e1bc1ea3353adec487a8c76ab8f8f35dc3e9e16c
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-28 21:04:10 +0500

    Addded migrations for email sign up

--- db/migrate/20251111074649_add_confirmable_to_users.rb
+++ db/migrate/20251111074649_add_confirmable_to_users.rb
@@ -1,7 +1,19 @@
+# db/migrate/XXXXXX_add_confirmable_to_users.rb
 class AddConfirmableToUsers < ActiveRecord::Migration[7.0]
-  def change
+  def up
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string
+    
+    add_index :users, :confirmation_token, unique: true
+    
+    # If you have existing users, confirm them automatically
+    User.update_all(confirmed_at: Time.now)
+  end
+  
+  def down
+    remove_columns :users, :confirmation_token, :confirmed_at, 
+                   :confirmation_sent_at, :unconfirmed_email
   end
 end
