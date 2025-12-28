3147f0def2b8c08737cc9fb3e8182f601a55f820
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-28 20:55:21 +0500

    Events controller after adding services to another folder.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,101 +2,27 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   
+  
   def index
-    @events = Event.includes(:category, :registrations).upcoming
+    result = Events::FilterService.call(params, current_user)
     
-    @user_location = get_user_location
-    
-    
-    if params[:category].present?
-      category = Category.find_by("LOWER(name) = ?", params[:category].downcase)
-      @events = @events.where(category_id: category.id) if category
-    end
-    
-    if params[:category_id].present? && params[:category_id] != ""
-      @events = @events.where(category_id: params[:category_id])
-    end
-    
-    if params[:city].present? && params[:city] != ""
-      @events = @events.where("location ILIKE ?", "%#{params[:city]}%")
-    end
-    
-    if params[:min_price].present?
-      @events = @events.where("price >= ?", params[:min_price])
-    end
-    
-    if params[:max_price].present?
-      @events = @events.where("price <= ?", params[:max_price])
-    end
-    
-    if params[:search].present?
-      search_term = "%#{params[:search]}%"
-      @events = @events.where(
-        "title ILIKE ? OR description ILIKE ? OR location ILIKE ?", 
-        search_term, search_term, search_term
-      )
-    end
-    
-    if @user_location.present?
-      lat = @user_location[:latitude]
-      lng = @user_location[:longitude]
-      radius = params[:radius]&.to_i || 50
-      
-      @events = @events.nearby(lat, lng, radius)
-      
-      @events = @events.to_a.map do |event|
-        event.define_singleton_method(:user_distance) do
-          distance_from(lat, lng)
-        end
-        event.define_singleton_method(:user_distance_formatted) do
-          distance_from_formatted(lat, lng)
-        end
-        event
-      end
-    else
-      @events = @events.order(date: :asc).to_a
-    end
-    
-    @cities = Event.pluck(:location)
-                   .map { |loc| loc.split(',').last&.strip }
-                   .uniq
-                   .compact
-                   .sort
-    
-    @categories = Category.all.order(:name)
+    @events = result.events
+    @categories = result.categories
+    @cities = result.cities
+    @user_location = result.user_location
   end
   
+  
   def show
-    @user_location = get_user_location
+    result = Events::ShowService.call(@event, params, current_user)
     
-    if @event.has_coordinates? && @user_location.present?
-      @distance = @event.distance_from(
-        @user_location[:latitude], 
-        @user_location[:longitude]
-      )
-      @distance_formatted = @event.distance_from_formatted(
-        @user_location[:latitude], 
-        @user_location[:longitude]
-      )
-    end
+    @user_location = result.user_location
+    @distance = result.distance
+    @distance_formatted = result.distance_formatted
+    @nearby_events = result.nearby_events
+    @registration = result.registration
     
-    if @event.has_coordinates?
-      @nearby_events = Event.where.not(id: @event.id)
-                           .nearby(@event.latitude, @event.longitude, 20)
-                           .limit(3)
-      
-      if @event.category_id.present?
-        @nearby_events = @nearby_events.where(category_id: @event.category_id)
-      end
-    end
-    
-    @registration = @event.registrations.find_by(user: current_user) if user_signed_in?
-    
-    if params[:success]
-      flash.now[:notice] = "Payment successful! Your ticket has been confirmed."
-    elsif params[:canceled]
-      flash.now[:alert] = "Payment was canceled."
-    end
+    handle_payment_flash
   end
   
   def new
@@ -114,16 +40,11 @@ class EventsController < ApplicationController
   end
   
   def edit
-    unless @event.user_id == current_user.id
-      redirect_to root_path, alert: "You don't have permission to edit this event."
-    end
+    authorize_event_owner!
   end
   
   def update
-    unless @event.user_id == current_user.id
-      redirect_to root_path, alert: "You don't have permission to edit this event."
-      return
-    end
+    authorize_event_owner!
     
     if @event.update(event_params)
       redirect_to @event, notice: 'Event was successfully updated.'
@@ -133,54 +54,25 @@ class EventsController < ApplicationController
   end
   
   def destroy
-    unless @event.user_id == current_user.id
-      redirect_to root_path, alert: "You don't have permission to delete this event."
-      return
-    end
+    authorize_event_owner!
     
     @event.destroy
     redirect_to events_url, notice: 'Event was successfully deleted.'
   end
   
+  
   def nearby
-    lat = params[:latitude].to_f
-    lng = params[:longitude].to_f
-    radius = params[:radius]&.to_i || 50
+    result = Events::NearbyService.call(params)
     
-    @events = Event.nearby(lat, lng, radius).limit(10)
-    
-    respond_to do |format|
-      format.json do
-        render json: @events.map { |event|
-          {
-            id: event.id,
-            title: event.title,
-            location: event.location,
-            latitude: event.latitude,
-            longitude: event.longitude,
-            price: event.price,
-            date: event.date,
-            distance: event.distance_from(lat, lng),
-            url: event_path(event)
-          }
-        }
-      end
-    end
+    render json: result.to_json
   end
   
   def search
     @query = params[:q]
-    @user_location = get_user_location
+    result = Events::SearchService.call(@query, current_user)
     
-    if @query.present?
-      @events = Event.search_with_location(
-        @query,
-        lat: @user_location&.dig(:latitude),
-        lng: @user_location&.dig(:longitude)
-      )
-    else
-      @events = Event.none
-    end
+    @events = result.events
+    @user_location = result.user_location
     
     render :index
   end
@@ -193,51 +85,22 @@ class EventsController < ApplicationController
   
   def event_params
     params.require(:event).permit(
-      :title, 
-      :description, 
-      :location, 
-      :date, 
-      :price, 
-      :category_id,
-      :latitude,
-      :longitude,
-      :image_url, 
-      :banner
+      :title, :description, :location, :date, :price, :category_id,
+      :latitude, :longitude, :image_url, :banner
     )
   end
   
-  def get_user_location
-    if params[:latitude].present? && params[:longitude].present?
-      return {
-        latitude: params[:latitude].to_f,
-        longitude: params[:longitude].to_f
-      }
+  def authorize_event_owner!
+    unless @event.user_id == current_user.id
+      redirect_to root_path, alert: "You don't have permission to perform this action."
     end
-    
-    if session[:user_latitude].present? && session[:user_longitude].present?
-      return {
-        latitude: session[:user_latitude].to_f,
-        longitude: session[:user_longitude].to_f
-      }
-    end
-    
-    default_locations = {
-      'Lahore' => { latitude: 31.5204, longitude: 74.3587 },
-      'Karachi' => { latitude: 24.8607, longitude: 67.0011 },
-      'Islamabad' => { latitude: 33.6844, longitude: 73.0479 }
-    }
-    
-    default_locations['Lahore']
   end
   
-  def store_location
-    if params[:latitude].present? && params[:longitude].present?
-      session[:user_latitude] = params[:latitude]
-      session[:user_longitude] = params[:longitude]
-      
-      render json: { success: true }
-    else
-      render json: { success: false }, status: :unprocessable_entity
+  def handle_payment_flash
+    if params[:success]
+      flash.now[:notice] = "Payment successful! Your ticket has been confirmed."
+    elsif params[:canceled]
+      flash.now[:alert] = "Payment was canceled."
     end
   end
 end
