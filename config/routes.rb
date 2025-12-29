9ee9339ad556ddff4734f78ebfccce3b2ba4296a
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-29 19:02:25 +0500

    integrated letter opener

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  
+
   root "pages#home"
+
+  # Letter Opener Web - View emails in development at /letter_opener
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   devise_for :users, controllers: {
     registrations: 'users/devise_registrations',
