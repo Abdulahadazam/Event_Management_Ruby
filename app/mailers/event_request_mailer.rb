8f711316b31c34da9a39b8c27bc6578b81a9aed2
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-30 11:32:39 +0500

    Added function of  event_request rejection email.

--- app/mailers/event_request_mailer.rb
+++ app/mailers/event_request_mailer.rb
@@ -1,15 +1,20 @@
-
 class EventRequestMailer < ApplicationMailer
-  default from: 'no-reply@example.com' # change
+  default from: 'noreply@eventhub.com'
 
   def submission_confirmation
     @event_request = params[:event_request]
-    mail(to: @event_request.email, subject: "We received your event request")
+    mail(to: @event_request.organizer_email, subject: "✅ We received your event request - EventHub")
   end
 
   def approved_email
     @event_request = params[:event_request]
     @event = params[:event]
-    mail(to: @event_request.email, subject: "Your event request has been approved")
+    mail(to: @event_request.organizer_email, subject: "🎉 Your event request has been approved! - EventHub")
+  end
+
+  def rejected_email
+    @event_request = params[:event_request]
+    @rejection_reason = params[:rejection_reason] || "Unfortunately, we cannot approve your event request at this time."
+    mail(to: @event_request.organizer_email, subject: "Event Request Update - EventHub")
   end
 end
