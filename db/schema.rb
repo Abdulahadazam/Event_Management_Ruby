27ecbf5cc1018a0606da5c704dc7c8bc14d9f1b3
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-30 15:37:37 +0500

    Added migration for fuzzy search gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_30_055701) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_30_082836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pg_trgm"
   enable_extension "postgis"
 
   create_table "active_admin_comments", force: :cascade do |t|
