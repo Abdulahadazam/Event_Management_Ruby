ba612ade1cf9da958fe2a3aefbc9ef7b82c0fe30
Abdulahadazam <abdulahadazam607@gmail.com>
2025-12-30 11:31:59 +0500

    Added functions of Action mailer to send emails when evet_reuqest Approved or rejected.

--- app/controllers/event_requests_controller.rb
+++ app/controllers/event_requests_controller.rb
@@ -6,19 +6,32 @@ class EventRequestsController < ApplicationController
   def create
     @event_request = EventRequest.new(event_request_params)
     if @event_request.save
-      redirect_to root_path, notice: "Your event request has been submitted successfully."
+      
+      EventRequestMailer.with(event_request: @event_request).submission_confirmation.deliver_later
+
+      redirect_to root_path, notice: "🎉 Thank you! Your event request has been submitted successfully. Our team will review it and contact you within 24-48 hours at #{@event_request.organizer_email}."
     else
       flash.now[:alert] = "Please correct the errors below."
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
-   
+
+  def show
+    @event_request = EventRequest.find(params[:id])
+  end
+
   private
 
   def event_request_params
     params.require(:event_request).permit(
-      :name, :email, :phone, :event_name, :event_type, :event_mode, 
-      :location, :date, :time, :expected_guests, :budget, :description, :special_requirements
+      :organizer_name, :organizer_email, :organizer_phone,
+      :event_title, :event_description, :category_id,
+      :preferred_date, :preferred_time,
+      :is_remote, :venue_address, :city, :country,
+      :platform, :meeting_link, :time_zone,
+      :ticket_price, :event_capacity,
+      :has_multiple_ticket_types, :standard_ticket_price, :vip_ticket_price, :premium_ticket_price,
+      :notes, :banner
     )
-  end   
+  end
 end
